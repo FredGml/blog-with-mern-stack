19eb139c9f5ca13eee77d4b8c2dec8e96111fdb1
Frédéric G. KOUTCHIHOUN <frederickoutchihoun@gmail.com>
2025-07-16 16:54:27 +0100

    Création des pages et des routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Accueil from './pages/Accueil'
+import Apropos from './pages/Apropos'
+import Connexion from './pages/Connexion'
+import Inscription from './pages/Inscription'
+import Dashboard from './pages/Dashbord'
+import Projets from './pages/Projets'
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-700'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/Apropos" element={<Apropos/>} />
+        <Route path="/Connexion" element={<Connexion/>} />
+        <Route path="/Inscription" element={<Inscription/>} />
+        <Route path="/Dashboard" element={<Dashboard/>} />
+        <Route path="/Projets" element={<Projets/>} />
+
+        </Routes>
+    
+    
+    </BrowserRouter>
   )
 }
