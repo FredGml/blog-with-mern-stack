b71082d1a9aeaa17338998d4150ab5d961196a76
Frédéric G. KOUTCHIHOUN <frederickoutchihoun@gmail.com>
2025-07-17 13:46:33 +0100

    Liaison à la base de donnée

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
 const express = require ('express')
+const mongoose = require ('mongoose')
+const dotenv = require ('dotenv')
+
+dotenv.config(); 
+
+mongoose.connect(
+    process.env.MONGO)
+.then(() => {
+    console.log('MongoDB connecté avec succès')
+})
+.catch((err) => {
+    console.log(err)
+});
+
 
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('Serveur lancé sur le port 3000');
-});
+})
